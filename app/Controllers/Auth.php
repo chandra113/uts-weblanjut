4dd52dbfa6bd8b71acb07957c19104feac2c7e47
firazyn <fyn99.178@gmail.com>
2020-12-04 09:46:47 +0700

    modify login and added register

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -5,9 +5,17 @@ class Auth extends BaseController
 	public function login()
 	{
 		$data = [
-			'title' => 'SIPUS - Login'
+			'title' => 'SIPUS | Login'
 		];
 		return view('auth/login', $data);
+	}
+
+	public function register()
+	{
+		$data = [
+			'title' => 'SIPUS | Register'
+		];
+		return view('auth/register', $data);
 	}
 
 	//--------------------------------------------------------------------
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,4 +1,4 @@
-<?= $this->extend('template/login_template'); ?>
+<?= $this->extend('template/reg_template'); ?>
 
 <?= $this->section('content'); ?>
 	<div class="limiter">
@@ -6,16 +6,25 @@
 			<div class="wrap-login100 p-b-160 p-t-50">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-43">
-						Login Sipus
+						Register Sipus
 					</span>
-					
 					<div class="wrap-input100 rs1 validate-input" data-validate = "Username is required">
+						<input class="input100" type="text" name="username">
+						<span class="label-input100">Nama Lengkap</span>
+					</div>
+
+					<div class="wrap-input100 rs2 validate-input" data-validate = "Valid email is required">
+						<input class="input100" type="email" name="email" pattern=".+@">
+						<span class="label-input100">Email</span>
+                    </div>
+                    
+					<div class="wrap-input100 validate-input" data-validate = "Username is required">
 						<input class="input100" type="text" name="username">
 						<span class="label-input100">Username</span>
 					</div>
 					
 					
-					<div class="wrap-input100 rs2 validate-input" data-validate="Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass">
 						<span class="label-input100">Password</span>
 					</div>
@@ -28,10 +37,10 @@
 					
 					<div class="text-center w-full p-t-23">
 						<span class="txt1">
-							Belum punya akun?
+							Sudah punya akun?
 						</span>
 						<a href="#" class="txt2">
-							Buat akun
+							Masuk disini
 						</a>
 					</div>
 				</form>
