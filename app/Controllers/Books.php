6840690627dedf1459260230819f8ed1195e42e8
firazyn <fyn99.178@gmail.com>
2020-12-06 13:01:16 +0700

    added search function

--- app/Controllers/Books.php
+++ app/Controllers/Books.php
@@ -8,14 +8,18 @@ class Books extends BaseController
 
 	public function index()
 	{
-		$book = $this->dummyBookModel->findAll();
+		$bookSearch = $this->request->getVar('search');
+
+		if ($bookSearch) {
+			$book = $this->dummyBookModel->search($bookSearch);
+		} else {
+			$book = $this->dummyBookModel;
+		}
 
 		$data = [
 			'title' => 'SIPUS | Available Books',
-			'book' => $book
+			'book' => $book->findAll()
 		];
-
-
 		return view('books/index', $data);
 	}
 
